3703705737773d338f8071b73fa6141db211af5e
Ray.Chang <b15982003@gmail.com>
2025-12-09 23:52:48 +0800

    release 1.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,15 +6,15 @@ plugins {
 
 android {
     namespace = "com.morefun.merit"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.morefun.merit"
         minSdk = 30
         //noinspection OldTargetApi
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
